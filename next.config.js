783bc08b43af0c0ba03fec9399d1c331696296f7
jonny <jonnykry93@gmail.com>
2023-11-22 16:57:34 -0800

    fix build yml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
