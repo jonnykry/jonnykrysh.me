ef115f1a54c8cb72298af0fbc15c36e7be60edfd
jonny <jonnykry93@gmail.com>
2023-11-22 16:46:44 -0800

    change output directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  distDir: 'dist',
 };
 
 module.exports = nextConfig;
