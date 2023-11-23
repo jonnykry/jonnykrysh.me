7b97e1beaebc709e171cb9e1d038be90905da397
jonny <jonnykry93@gmail.com>
2023-11-22 16:49:22 -0800

    another attempt at dist

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  distDir: 'dist',
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 };
 
 module.exports = nextConfig;
