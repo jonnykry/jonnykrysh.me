ec5241e7f32c037d82d847ab15a57a4464941d31
jonny <jonnykry93@gmail.com>
2023-11-28 14:16:14 -0800

    fix headers for hsts

--- next.config.js
+++ next.config.js
@@ -1,15 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   images: {
     unoptimized: true,
   },
-  headers: [
-    {
-      key: 'Strict-Transport-Security',
-      value: 'max-age=63072000; includeSubDomains; preload',
-    },
-  ],
 };
 
 module.exports = nextConfig;
