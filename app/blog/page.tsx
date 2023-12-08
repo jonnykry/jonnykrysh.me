79ae1146dd8b3e7c927c46a20f08685c0756ebf7
jonny <jonnykry93@gmail.com>
2023-12-08 15:59:49 -0800

    those unused vars will always get me

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,6 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 import Link from 'next/link';
-import { LoadingSpinner } from '../components/LoadingSpinner';
 
 const getPosts = async () => {
   const postsDirectory = path.join(process.cwd(), 'app', '_posts');
